27c59d8edac92929cb10bd43b0d0c3d00ce80ef7
zhan <zhunussov1994@gmail.com>
2019-07-30 15:05:05 +0600

    Second commit

--- remove_digit.py
+++ remove_digit.py
@@ -4,13 +4,13 @@ string = 'Alice is 21 years old'
 
 def nodigits(string):
     pattern = '\d'
-    nodigit_list = re.split(pattern, string)
-    result = ' '.join(nodigit_list)
-    print(result.replace('    ',' '))
-    return result
+    nodigit_list = re.sub(pattern, '', string)
+    
+    return nodigit_list
+
+print(nodigits(string))
+assert nodigits(string) == 'Alice is  years old'
+assert nodigits('3 times a day he drinks 2 liters of water') == ' times a day he drinks  liters of water'
+assert nodigits('Bob is 30 years old and have 3 cats') == 'Bob is  years old and have  cats'
 
 
-assert nodigits(string) == 'Alice is    years old'
-assert nodigits('10 days she had to attend school') == '   days she had to attend school'
-assert nodigits('John is 18 years old and he has 5 cats') == 'John is    years old and he has   cats'
-assert nodigits('We are going to buy 10 orenges from 5 different shops') == 'We are going to buy    orenges from   different shops'
